41ff07a604a1a62369ba817f8cdc32064611f0d0
jungsungh <jungsungh0924@gmail.com>
2024-01-13 14:36:08 +0900

    로그인 UI 완료

--- login/src/component/login/Login.js
+++ login/src/component/login/Login.js
@@ -1,6 +1,8 @@
 import './login.css';
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
+//import {Link} from 'react-router-dom';
 //import {reactComponent as Icon} from "http://www.w3.org/2000/svg";
 //import user_icon from 'http://www.w3.org/2000/svg'
 //import MaterialIcon from 'react-google-material-icons'
@@ -31,6 +33,15 @@ const Login = () => {
             </div>
 
             <button type='submit'>로그인</button>
+
+            <div className='searchbox'>
+                <Link to={'https://www.naver.com'}> 아이디 찾기 </Link>
+                <Link>|</Link>
+                <Link to={'https://www.daum.net'}> 비밀번호 찾기 </Link>
+                <Link>|</Link>
+                <Link to={'https://www.google.com'}> 회원가입 </Link>
+            </div>
+            
             
             </form>
             </div>
