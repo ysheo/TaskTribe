e5501080f9a36eca4334d10bf5edbc7da53b7dcc
jungsungh <jungsungh0924@gmail.com>
2024-02-01 08:17:01 +0900

    아이디 찾기 생성 중

--- frontend/login/src/pages/Search.js
+++ frontend/login/src/pages/Search.js
@@ -1,7 +1,20 @@
+// import { useState } from "react";
+import "../css/search.css";
+// import { FaUser } from "react-icons/fa";
+// import { RiLockPasswordLine } from "react-icons/ri";
+// import { useNavigate, useParams } from "react-router-dom";
+
 const Search = () => {
     return(
-        <div className="Search">
-            아이디/비밀번호 찾기
+        <div className="search">
+            <form>
+            <h3 className="idsearch">아이디 찾기</h3>
+            <div className="searchbox">
+            
+            <button type="click">아이디 찾기</button>
+
+            </div>
+            </form>
         </div>
     )
 }
