bacbcce1affbb7a61dfe9480cfcc277ec433cc08
dada-see <dleksk12@naver.com>
2024-07-01 01:34:14 +0900

    Body.js 삭제 후 App.js로 Router 이동

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import Body from './component/Body';
+import React from "react";
+import Login from "./Login";
+import Join from "./join/Join";
+import Main from "./Main";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Body/>
+            <div className="Body">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Login/>} />
+                        <Route path="/join" element={<Join/>} />
+                        <Route path="/main" element={<Main/>} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
         </div>
     );
 }
